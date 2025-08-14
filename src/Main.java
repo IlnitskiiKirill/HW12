f5c33ed754b649edc8bf7ecea2acaea393aec3fa
hello word <ilnitskiykirill@gmail.com>
2025-08-14 20:47:11 +0300

    исправил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,28 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         // Тестирование задачи 1
-        {
+
             System.out.println("Задача №1");
-        }
+
         checkLeapYear(2020);
         checkLeapYear(2021);
 
         // Тестирование задачи 2
-        {
+
             System.out.println("Задача №2");
-        }
+
         suggestAppVersion(0, 2014); // iOS, старый телефон
         suggestAppVersion(1, 2016); // Android, новый телефон
         suggestAppVersion(0, 2023); // iOS, текущий год
 
         // Тестирование задачи 3
-        {
-            System.out.println("Задача №3");
-        }
-        System.out.println("Потребуется дней: " + calculateDeliveryDays(95));
-        System.out.println("Потребуется дней: " + calculateDeliveryDays(15));
-        System.out.println("Потребуется дней: " + calculateDeliveryDays(65));
-        System.out.println("Потребуется дней: " + calculateDeliveryDays(150));
+        System.out.println("Задача №3");
+        calculateAndPrintDeliveryDays(95);
+        calculateAndPrintDeliveryDays(15);
+        calculateAndPrintDeliveryDays(65);
+        calculateAndPrintDeliveryDays(150);
     }
 
     // Задача 1: Проверка високосного года
@@ -47,16 +45,25 @@ public class Main {
         System.out.println("Установите " + versionType + " версию приложения для " + osName + " по ссылке");
     }
 
-    // Задача 3: Расчет дней доставки
-    public static int calculateDeliveryDays(int deliveryDistance) {
-        if (deliveryDistance <= 20) {
-            return 1;
-        } else if (deliveryDistance <= 60) {
-            return 2;
-        } else if (deliveryDistance <= 100) {
-            return 3;
-        } else {
-            return -1; // или можно выбросить исключение, если доставка невозможна
+    // Задача 3: Расчет дней доставки (исправленная версия)
+    public static void calculateAndPrintDeliveryDays(int deliveryDistance) {
+        int deliveryDays = 1; // Минимальное количество дней
+
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки на расстояние " + deliveryDistance + " км нет");
+            return;
         }
+
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
 }
+
+
+
